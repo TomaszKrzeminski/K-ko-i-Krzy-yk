7541fedf13e669ae3ef4b896b523267190787c2a
Tomasz Krzeminski <koral2323@gmail.com>
2020-03-15 18:16:00 +0100

    First commit

---
+++ K_K.js
@@ -0,0 +1,66 @@
+﻿$(document).ready(function () {
+
+    var players = [{ name: 'Kółko' }, { name: 'Krzyżyk' }];
+
+    var currentPlayer = players[0];
+
+    var board = [
+
+        [null, null, null],
+        [null, null, null],
+        [null, null, null]
+
+
+    ];
+
+
+    function displayCurrentPlayer() {
+
+        $('.status').text('Bieżący gracz : ' + currentPlayer.name);
+
+    }
+
+    displayCurrentPlayer();
+
+
+   
+
+
+    function move() {
+        $('.board').on('click', '.cell', function () {
+
+
+            if ($(this).hasClass('cell_O') === false && $(this).hasClass('cell_X') === false)
+            {
+                if (currentPlayer.name === 'Kółko') {
+                    $(this).addClass('cell_O');
+                    currentPlayer = players[1];
+
+                }
+                else {
+
+                    $(this).addClass('cell_X');
+                    currentPlayer = players[0];
+                }
+            }
+            
+               
+           
+
+
+
+
+           
+
+        });
+        //checkWin();
+        displayCurrentPlayer();
+    }
+
+    move();
+
+
+
+
+
+});
