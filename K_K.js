149eee8137a042a6f278d4829e867d65f845fc73
Tomasz Krzeminski <koral2323@gmail.com>
2020-03-18 18:58:29 +0100

    Working

--- K_K.js
+++ K_K.js
@@ -4,14 +4,16 @@
 
     var currentPlayer = players[0];
 
-    var board = [
+    var count = 0;
 
-        [null, null, null],
-        [null, null, null],
-        [null, null, null]
+    var BoardOfCells = [
 
+        ["", "", ""],
+        ["", "", ""],
+        ["", "", ""]
 
     ];
+
 
 
     function displayCurrentPlayer() {
@@ -22,39 +24,243 @@
 
     displayCurrentPlayer();
 
+    $('.win').dialog({
+        autoOpen: false, close:function() {
+            End();
+        }
+    });
 
-   
+
+    function End() {
+
+       
+        count = 0;
+        currentPlayer = players[0];
+        BoardOfCells = BoardOfCells = [
+
+            ["", "", ""],
+            ["", "", ""],
+            ["", "", ""]
+
+        ];
+        $('.cell').each(function () {
+
+            $(this).removeClass('cell_X');
+            $(this).removeClass('cell_O');
+
+
+        });
+       
+
+    }
+
+
+    function Win(sign)
+    {
+        var text;
+        if (sign === 'O') {
+            text = "Kółko";
+        }
+        else
+        {
+            text = "Krzyżyk";
+        }
+        $('.cell').show("explode", { pieces: 16 }, 2000);
+        $(".win").text("Wygrał gracz "+text);
+        $('.win').dialog('open');
+        End();
+        
+
+
+    }
+
+    $('.end').dialog({
+        autoOpen: false, close: function () {
+            End();
+        }});
+
+    function CheckEnd()
+    {
+
+        if (count === 9)
+        {
+            $('.end').dialog('open');
+            End();
+           
+        }
+
+    }
+
+
+    function SetBoard() {
+
+        var cells = document.getElementsByClassName("cell");
+        var k = 0;
+        var check;
+
+
+
+        for (var i = 0; i < 3; i++) {
+
+
+            for (var j = 0; j < 3; j++) {
+                check = cells[k];
+
+                if (check.classList.contains('cell_O')) {
+                    BoardOfCells[i][j] = 'O';
+                }
+                else if (check.classList.contains('cell_X')) {
+                    BoardOfCells[i][j] = 'X';
+                }
+
+
+                k++;
+
+
+            }
+
+
+        }
+
+
+
+
+    }/////
+
+
+    function CheckWinHorizontally(sign) {
+
+        for (var i = 0; i < 3; i++) {
+
+            k = 0;
+            for (var j = 0; j < 3; j++) {
+
+                if (BoardOfCells[i][j] === sign) {
+                    k++;
+                }
+
+            }
+
+            if (k === 3) {
+                Win(sign);
+                break;
+            }
+
+
+        }
+
+
+
+
+
+
+    }
+
+    function CheckWinPerpendicularly(sign) {
+
+
+        for (var i = 0; i < 3; i++) {
+            var k = 0;
+            for (var j = 0; j < 3; j++) {
+
+
+                if (BoardOfCells[j][i] === sign) {
+                    k++;
+                }
+
+                if (k === 3) {
+                    Win(sign);
+                    break;
+                }
+
+            }
+
+
+
+        }
+
+
+
+    }
+
+    function CheckWinSlant(sign) {
+
+        var l = 0;
+        var p = 0;
+        var length = BoardOfCells.length;
+
+        for (var i = 0; i < 3; i++) {
+
+            if (BoardOfCells[i][i] === sign) {
+                l++;
+            }
+
+            if (l === 3) {
+                Win(sign);
+                break;
+            }
+
+
+        }
+
+
+      
+
+        var x = BoardOfCells.length - 1;
+        var y = 0;
+        while (x >= 0) {
+
+            if (BoardOfCells[y][x] === sign) {
+                p++;
+            }
+
+            if (p === 3) {
+                Win(sign);
+                break;
+            }
+            x--;
+            y++;
+
+
+        }
+
+
+
+    }
 
 
     function move() {
         $('.board').on('click', '.cell', function () {
 
-
-            if ($(this).hasClass('cell_O') === false && $(this).hasClass('cell_X') === false)
-            {
+            var sign;
+            if ($(this).hasClass('cell_O') === false && $(this).hasClass('cell_X') === false) {
                 if (currentPlayer.name === 'Kółko') {
                     $(this).addClass('cell_O');
                     currentPlayer = players[1];
+                    sign = 'O';
 
                 }
                 else {
 
                     $(this).addClass('cell_X');
                     currentPlayer = players[0];
+                    sign = 'X';
+
                 }
             }
+
+            SetBoard();
+            CheckWinHorizontally(sign);
+            CheckWinPerpendicularly(sign);
+            CheckWinSlant(sign);
+            count++;
+            CheckEnd();
+            displayCurrentPlayer();
             
-               
-           
-
-
-
-
-           
-
         });
-        //checkWin();
-        displayCurrentPlayer();
+
+
+
     }
 
     move();
